d3a8c9e1f58fe0957c5427c97086e68dffd8cc60
Douglas Wilson <dougwilson0000@yahoo.com>
2020-07-14 15:19:11 -0400

    quick fix

--- assets/scripts/Traveler.js
+++ assets/scripts/Traveler.js
@@ -7,22 +7,22 @@ class Traveler {
     }
 
     hunt() {
-        this.isHealthy = true
+        
        return this.food = this.food + 2
     }
     eat() {
         if(this.food > 1) {
             this.food = this.food -1
-            this.isHealthy = true
+            
             return
         }
-    //    (this.food > 1) ? this.food = this.food - 1
+   
         if(this.food === 1) {
             this.food = this.food - 1
-            this.isHealthy = false
+            
             return
         }
-    //    (this.food === 1) ? this.food = this.food - 1 this.isHealthy = false
+
         if(this.food === 0) {
             this.isHealthy = false
             return
